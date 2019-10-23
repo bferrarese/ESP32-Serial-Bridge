c28b41b608f4a624e8c0e4ffdf5f76e4436c4341
SchumyHao <schumyhaojl@126.com>
2019-10-23 15:31:52 +0800

    Format code
    Signed-off-by: SchumyHao <schumyhaojl@126.com>

--- src/config.h
+++ src/config.h
@@ -4,26 +4,30 @@
 #define MAX_NMEA_CLIENTS 4
 //#define relay1 19
 //#define relay2 18
-const char* ssid     = "";
-const char* password = "";
-const char* MQTT_server= "";
-const char* MQTT_user= "";
-const char* MQTT_pass= "";
-const char* ClientID= "Esp32";
-const char* pubTopic= "Esp32/result";
-const char* subTopic= "Esp32/cmd";
-unsigned char qos= 1; //subscribe qos
-bool retained= false;
+const char *ssid = "";
+const char *password = "";
+const char *MQTT_server = "";
+const char *MQTT_user = "";
+const char *MQTT_pass = "";
+const char *ClientID = "Esp32";
+const char *pubTopic = "Esp32/result";
+const char *subTopic = "Esp32/cmd";
+unsigned char qos = 1; //subscribe qos
+bool retained = false;
+
+#if (DEBUG)
+bool debug = true;
+#else
 bool debug = false;
+#endif
 
 /*************************  COM Port 0 *******************************/
-#define UART_BAUD0 9600            // Baudrate UART0
-#define SERIAL_PARAM0 SERIAL_8N1    // Data/Parity/Stop UART0
-#define SERIAL0_RXPIN 21            // receive Pin UART0
-#define SERIAL0_TXPIN 1             // transmit Pin UART0
-#define SERIAL0_TCP_PORT 8880       // Wifi Port UART0
+#define UART_BAUD0 9600          // Baudrate UART0
+#define SERIAL_PARAM0 SERIAL_8N1 // Data/Parity/Stop UART0
+#define SERIAL0_RXPIN 21         // receive Pin UART0
+#define SERIAL0_TXPIN 1          // transmit Pin UART0
+#define SERIAL0_TCP_PORT 8880    // Wifi Port UART0
 
 #define bufferSize 1024
 
 //////////////////////////////////////////////////////////////////////////
-
